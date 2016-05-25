0a509002f28ef09774d5285ffd1d425782ec5478
cochransean <cochransean@gmail.com>
2016-05-24 22:59:34 -0400

    minor edit to get staticfiles to work

--- storyStack/settings.py
+++ storyStack/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     }
 }
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
