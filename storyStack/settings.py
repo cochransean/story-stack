e6cb2c2ef377d0842244d3925871112d3d9cb687
cochransean <cochransean@gmail.com>
2016-05-24 23:02:14 -0400

    minor edit to get staticfiles to work

--- storyStack/settings.py
+++ storyStack/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'storyStack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
-    'default': {}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+    }
 }
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
