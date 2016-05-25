e93e3b1aba37976e734bd87ae856eec2f1b12e89
cochransean <cochransean@gmail.com>
2016-05-25 12:55:08 -0400

    added .keep for heroku

--- storyStack/urls.py
+++ storyStack/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^stack/', include('stack.urls')),
-
-    # I am temporarily having main page redirect to stack application for testing
-    url(r'^$', include('stack.urls')),
     url(r'^admin/', admin.site.urls),
 ]
