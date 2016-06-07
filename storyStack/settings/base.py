045382834c73203d83f6f025c033e18966ae5620
cochransean <cochransean@gmail.com>
2016-06-07 14:55:36 -0400

    added pre-build script to fix error on Heroku build

--- storyStack/settings/base.py
+++ storyStack/settings/base.py
@@ -111,6 +111,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/bower_components'),
 ]
 
 # Simplified static file serving.
