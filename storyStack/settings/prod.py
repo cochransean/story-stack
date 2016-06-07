92e2474c8663c3970beee4df2029d7f6ddd4cfc5
cochransean <cochransean@gmail.com>
2016-06-07 16:16:58 -0400

    fixed allowed hosts setting

--- storyStack/settings/prod.py
+++ storyStack/settings/prod.py
@@ -17,3 +17,5 @@ DATABASES['default'].update(db_from_env)
 
 # Allow serving from Heroku
 ALLOWED_HOSTS = ['*']
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
