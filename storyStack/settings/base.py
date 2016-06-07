069413bcec4e10d03a5b365ec0dedc3e4607c2d0
cochransean <cochransean@gmail.com>
2016-06-07 15:54:06 -0400

    switched from bower to entirely npm for dependencies

--- storyStack/settings/base.py
+++ storyStack/settings/base.py
@@ -110,8 +110,8 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'static/bower_components'),
+    os.path.join(BASE_DIR, 'node_modules'),
+    os.path.join(PROJECT_ROOT, 'static')
 ]
 
 # Simplified static file serving.
