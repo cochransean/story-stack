65ff0c6e20579a366a0589157f0ad688b359a2fb
cochransean <cochransean@gmail.com>
2016-06-07 14:32:28 -0400

    split settings into dev / production

--- storyStack/wsgi.py
+++ storyStack/wsgi.py
@@ -12,7 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storyStack.settings.prod")
+# Note that DJANGO settings variable is set in package.json upon buildout of dependencies in Heroku, to prevent
+# Heroku-specific errors
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
